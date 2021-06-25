3bf20b1868518c1f44f058053f53a2b71df0e9cd
Lucas Neris <lucas.lima.neris@gmail.com>
2021-06-25 16:45:40 -0300

    responsiveness adjustments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 window.addEventListener('load', activeToggle)
 
 function activeToggle() {
-    let menuToggle = document.querySelector('#container .menu-toggle')
+    let menuToggle = document.querySelector('#container .menu-toggle-container')
 
     menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active')
+        
         let menu = document.querySelector('#container .menu')
         menu.classList.toggle('active')
     })   
+
+    disableToggle()
+}
+
+function disableToggle() {
+    let destinations = document.querySelector('#container #a-destinations')
+    let prices = document.querySelector('#container #a-prices')
+
+    eventDisable(destinations)
+    eventDisable(prices)
+}
+
+function eventDisable(element) {
+    element.addEventListener('click', () => {
+        let menuToggle = document.querySelector('#container .menu-toggle-container')
+        let menu = document.querySelector('#container .menu')
+
+        menuToggle.classList.toggle('active')
+        menu.classList.toggle('active')
+    })
 }
